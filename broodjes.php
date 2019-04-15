ffd5f07a8f2b3d5ab64893317b78603e1ac31eb3
Patrick <42439682+Spyrincho@users.noreply.github.com>
2019-04-15 09:20:17 +0200

    Create broodjes.php

---
+++ broodjes.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <title>Broodjes bestellen!!</title>
+</head>
+<body>
+
+<h2>Broodjes Bongusta Sloterdijk</h2>
+
+    <a href="broodjes_upload.php">klik hier om te uploaden</a>
+    <br>
+
+<table>
+    <thead>
+    <tr>
+        <th>Broodje</th>
+        <th>Prijs</th>
+        <th>Omschrijving</th>
+        <th>Afbeelding</th>
+    </tr>
+    </thead>
+
+
+
+
+</table>
+
+</body>
+</html>
